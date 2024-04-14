0fbe09f61ef3f36d638871a571ce1190651108f6
NUBcore <beau@nubcore.com>
2024-04-14 14:59:25 -0700

    Update project.v to Wokwi
    Updating mapping to be Wokwi compatible
    
    Derived from TinyTapeout/tt03-test-7seg/blob/main/src/cells.v

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Beau Ambur, DEFAULT
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -21,4 +21,99 @@ module tt_um_nubcore_default_tape (
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+endmodule
+
+module buffer_cell (
+    input wire in,
+    output wire out
+    );
+    assign out = in;
+endmodule
+
+module and_cell (
+    input wire a,
+    input wire b,
+    output wire out
+    );
+
+    assign out = a & b;
+endmodule
+
+module or_cell (
+    input wire a,
+    input wire b,
+    output wire out
+    );
+
+    assign out = a | b;
+endmodule
+
+module xor_cell (
+    input wire a,
+    input wire b,
+    output wire out
+    );
+
+    assign out = a ^ b;
+endmodule
+
+module nand_cell (
+    input wire a,
+    input wire b,
+    output wire out
+    );
+
+    assign out = !(a&b);
+endmodule
+
+module not_cell (
+    input wire in,
+    output wire out
+    );
+
+    assign out = !in;
+endmodule
+
+module mux_cell (
+    input wire a,
+    input wire b,
+    input wire sel,
+    output wire out
+    );
+
+    assign out = sel ? b : a;
+endmodule
+
+module dff_cell (
+    input wire clk,
+    input wire d,
+    output reg q,
+    output wire notq
+    );
+
+    assign notq = !q;
+    always @(posedge clk)
+        q <= d;
+
+endmodule
+
+module dffsr_cell (
+    input wire clk,
+    input wire d,
+    input wire s,
+    input wire r,
+    output reg q,
+    output wire notq
+    );
+
+    assign notq = !q;
+
+    always @(posedge clk or posedge s or posedge r) begin
+        if (r)
+            q <= 0;
+        else if (s)
+            q <= 1;
+        else
+            q <= d;
+    end
 endmodule
