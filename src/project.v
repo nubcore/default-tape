ec0cc80421a400c21a1f2c37535a873e1604a6f3
NUBcore <beau@nubcore.com>
2024-04-14 17:20:39 -0700

    added "#( parameter MAX_COUNT = 24'd10_000_000 )".

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_nubcore_default_tape (
+module tt_um_nubcore_default_tape #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
